7487f2207c5b12507f206958ba138a43fdcfeb68
Peter Christensen <peterchristensen@Peters-MacBook-Pro.local>
2019-05-21 12:35:45 -0600

    finished mini project

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const {SERVER_PORT, SESSION_SECRET} = process.env
-const msg_ctrl = require('./messagesCtrl')
+const messagesCtrl = require('./messagesCtrl')
 const session = require('express-session')
 
 app.use(session({
@@ -15,7 +15,21 @@ app.use(session({
     }
 }))
 
+app.use((req, res, next) => {
+    let badWords = ['knucklehead', 'jerk', 'internet explorer']
+    if(req.body.message) {
+        for (let i = 0; i < badWords.length; i++) {
+            let regex = new RegExp(badWords[i], 'g')
+            req.body.message = req.body.message.replace(regex, '****')
+        }
+        next()
+    } else {
+        next()
+    }
+})
+
 app.listen(SERVER_PORT, () => console.log(`magic is happening on ${SERVER_PORT}`))
 
-app.get('/api/messages', messageCtrl.getAllMessages)
-app.post('/api/message', messageCtrl.createMessage)
+app.get('/api/messages', messagesCtrl.getAllMessages)
+app.get('/api/messages/history', messagesCtrl.history)
+app.post('/api/message', messagesCtrl.createMessage)
