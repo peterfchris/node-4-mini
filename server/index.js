70da09e34af69a8cc12a4fbf73a8950dd0dfddeb
Peter Christensen <peterchristensen@Peters-MacBook-Pro.local>
2019-05-21 12:04:56 -0600

    up through step 5

--- server/index.js
+++ server/index.js
@@ -3,5 +3,19 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const {SERVER_PORT, SESSION_SECRET} = process.env
+const msg_ctrl = require('./messagesCtrl')
+const session = require('express-session')
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}))
 
 app.listen(SERVER_PORT, () => console.log(`magic is happening on ${SERVER_PORT}`))
+
+app.get('/api/messages', messageCtrl.getAllMessages)
+app.post('/api/message', messageCtrl.createMessage)
---
+++ server/messagesCtrl.js
@@ -0,0 +1,16 @@
+const allMessages = []
+
+module.exports = {
+    getAllMessages: (req, res) => {
+        res.status(200).send(allMessages)
+    },
+    createMessage: (req, res) => {
+        const {username, message} = req.body
+        let newMessage = {
+            username,
+            message
+        }
+        allMessages.push(newMessage)
+        res.status(200).send(allMessages)
+    }
+}
